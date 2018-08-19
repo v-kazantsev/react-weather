d869d094a2e266b82b7e26eacb33786b70df972f
v-kazantsev <vvs201@mail.ru>
2018-08-19 13:44:09 +0900

    Edited readme

--- src/elements/LeftPane.js
+++ src/elements/LeftPane.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import styled from 'styled-components';
-import selectIcon from 'components/selectIcon';
+import { Cloudy, Default, Misty, Partly, Rainy, Snow, Sunny, Wind } from 'images';
 
 const Pane = styled.div`
   flex: 1;
   margin: 2em 0.5em;
 `;
+const selectIcon = (condition) => {
+  const str = condition;
+  switch (true) {
+    case /clear/i.test(str):
+    return Sunny;
+    case /cloud/i.test(str):
+    return Partly;
+    case /scattered/i.test(str):
+    return Cloudy;
+    case /fog/i.test(str):
+    return Misty;
+    case /rain/i.test(str):
+    return Rainy;
+    case /snow/i.test(str):
+    return Snow;
+    case /wind/i.test(str):
+    return Wind;
+    default:
+    return Default;
+  }
+};
 
 const LeftPane = (props) => (
   
--- src/elements/RightPane.js
+++ src/elements/RightPane.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import React from 'react';
+import Chartist from 'chartist';
 import ChartistGraph from 'react-chartist';
 
 const Pane = styled.div`
@@ -13,13 +14,14 @@ margin: 2em 0.5em;
 class RightPane extends React.Component {
   render() {
     const chartData = {
-      series: [[1030, 1010, 1000, 990, 980]],
+      series: [[1030, 1010, 1000, 990, 1010]],
       labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri']
     };
     const type = 'Line';
     const chartOptions = {
+      showPoint: true,
       low: 900,
-      showArea: false,
+      lineSmooth: Chartist.Interpolation.none()
     }
     return (
       <Pane>
